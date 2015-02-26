7100516a3fcdc464a17cad87d5139039602c5935
Ian Kronquist <iankronquist@teleport.com>
2015-02-26 13:16:27 -0800

    Auto-generate cookbook with berks

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'create'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures create'
+long_description 'Installs/Configures create'
+version          '0.1.0'
