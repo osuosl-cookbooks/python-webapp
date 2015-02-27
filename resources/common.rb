1af15da06b2bebe715c89d8bca3ba95f949dd15e
Ian Kronquist <iankronquist@teleport.com>
2015-02-26 17:32:41 -0800

    Start defining attributes for common resource

---
+++ resources/common.rb
@@ -0,0 +1,17 @@
+require 'git'
+
+actions :install
+default_action :install
+
+attribute :path, 'kind_of' => String, :default => '/opt/app/'
+attribute :owner, 'kind_of' => String, :default => 'chef'
+attribute :group, 'kind_of' => String, :default => 'chef'
+
+attribute :git_repo, 'kind_of' => String
+attribute :git_branch, 'kind_of' => String, :default => 'master'
+
+attribute :config_template, 'kind_of' => String,
+                            :default => 'templates/default/settings.py.erb'
+attribute :config_destination, 'kind_of' => String,
+                               :default => '/opt/app/settings.py'
+attribute :config_vars, 'kind_of' => Hash
