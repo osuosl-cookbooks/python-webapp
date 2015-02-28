5855129efd35e0c8ca6c871accd999811302c311
Ian Kronquist <iankronquist@teleport.com>
2015-02-27 17:13:32 -0800

    Fix rubocop metadata quote style errors

--- test/cookbooks/python-webapp-test/metadata.rb
+++ test/cookbooks/python-webapp-test/metadata.rb
@@ -1,9 +1,9 @@
-name "python-webapp-test"
-maintainer "OSU Open Source Lab"
-maintainer_email "support@osuosl.org"
-licens "Apache 2.0"
-description "Used to test the python-webapp-cookbook cookbook"
+name 'python-webapp-test'
+maintainer 'OSU Open Source Lab'
+maintainer_email 'support@osuosl.org'
+licens 'Apache 2.0'
+description 'Used to test the python-webapp-cookbook cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "1.0.0"
+version '1.0.0'
 
-depends "python-webapp-cookbook"
+depends 'python-webapp-cookbook'
