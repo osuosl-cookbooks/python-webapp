8243950cc85c15313be12f89b704b440348a848d
Ian Kronquist <iankronquist@teleport.com>
2015-02-28 13:37:15 -0800

    Specify behavior when config_template is nil

--- resources/common.rb
+++ resources/common.rb
@@ -14,6 +14,7 @@ attribute :git_repo, 'kind_of' => String
 attribute :git_branch, 'kind_of' => String, :default => 'master'
 
 # Specify the config file template, destination on the node, and variables
+# If :config_template is nil, do nothing
 attribute :config_template, 'kind_of' => String,
                             :default => 'settings.py.erb'
 attribute :config_destination, 'kind_of' => String,
