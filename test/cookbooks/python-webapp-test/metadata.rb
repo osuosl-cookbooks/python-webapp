247ee21b0ec361ffc485caf361de39a13298721c
Ian Kronquist <iankronquist@teleport.com>
2015-02-27 17:48:09 -0800

    Fix typo in cookbook metadata
    'licens' should be 'license'. Oops, my bad.

--- test/cookbooks/python-webapp-test/metadata.rb
+++ test/cookbooks/python-webapp-test/metadata.rb
@@ -1,7 +1,7 @@
 name 'python-webapp-test'
 maintainer 'OSU Open Source Lab'
 maintainer_email 'support@osuosl.org'
-licens 'Apache 2.0'
+license 'Apache 2.0'
 description 'Used to test the python-webapp-cookbook cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
