bc6af59c0823926d49f0c70acb18c99717025547
Ian Kronquist <iankronquist@teleport.com>
2015-03-05 12:45:24 -0800

    Change maintainer email

--- test/cookbooks/python-webapp-test/metadata.rb
+++ test/cookbooks/python-webapp-test/metadata.rb
@@ -1,6 +1,6 @@
 name 'python-webapp-test'
 maintainer 'OSU Open Source Lab'
-maintainer_email 'support@osuosl.org'
+maintainer_email 'chef@osuosl.org'
 license 'Apache 2.0'
 description 'Used to test the python-webapp cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
