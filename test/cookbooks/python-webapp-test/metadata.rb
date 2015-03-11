2e58f4fcc7c7722d3476065f924134826595fa8c
Ian Kronquist <iankronquist@teleport.com>
2015-03-10 17:49:55 -0700

    Rename cookbook in metadata

--- test/cookbooks/python-webapp-test/metadata.rb
+++ test/cookbooks/python-webapp-test/metadata.rb
@@ -6,4 +6,4 @@ description 'Used to test the python-webapp cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
-depends 'python-webapp-cookbook'
+depends 'python-webapp'
