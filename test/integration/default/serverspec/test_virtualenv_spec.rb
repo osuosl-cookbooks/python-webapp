46990d8a2351934d21a7bd1cd2706a20921972cc
Ian Kronquist <iankronquist@teleport.com>
2015-03-19 13:54:22 -0700

    Test new LWRP invocation

--- test/integration/default/serverspec/test_virtualenv_spec.rb
+++ test/integration/default/serverspec/test_virtualenv_spec.rb
@@ -6,3 +6,10 @@ describe file('/opt/venv/') do
   it { should be_grouped_into 'whats_fresh' }
   it { should be_owned_by('whats_fresh') }
 end
+
+# Test that the virtualenv's directory is properly set up
+describe file('/opt/venv_h2o/') do
+  it { should be_directory }
+  it { should be_grouped_into 'working_waterfronts' }
+  it { should be_owned_by('working_waterfronts') }
+end
