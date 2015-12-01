e962e2f3344ec0dd5d66230ebfd52d340f4a6c21
Evan Tschuy <tschuy@osuosl.org>
2015-12-01 15:55:43 -0800

    convert resources to symbols

--- resources/default.rb
+++ resources/default.rb
@@ -20,43 +20,43 @@
 default_action :install
 
 # Creates the user if they do not already exist
-attribute :create_user, 'kind_of' => [TrueClass, FalseClass], :default => false
+attribute :create_user, kind_of: [TrueClass, FalseClass], default: false
 
 # Create the owner, path, or group if they do not exist
 # If path is nil it will default to '/opt/<name_attribute>'
-attribute :path, 'kind_of' => [String, NilClass], :default => nil
-attribute :owner, 'kind_of' => String, :default => 'chef'
-attribute :group, 'kind_of' => String, :default => 'chef'
+attribute :path, kind_of: [String, NilClass], default: nil
+attribute :owner, kind_of: String, default: 'chef'
+attribute :group, kind_of: String, default: 'chef'
 
 # The information necessary to check out the code
-attribute :repository, 'kind_of' => String
-attribute :revision, 'kind_of' => String, :default => 'master'
-attribute :managepy, 'kind_of' => String, :default => 'manage.py'
+attribute :repository, kind_of: String
+attribute :revision, kind_of: String, default: 'master'
+attribute :managepy, kind_of: String, default: 'manage.py'
 
 # Location of the virtualenv
-attribute :virtualenv_path, 'kind_of' => [String, NilClass], :default => nil
+attribute :virtualenv_path, kind_of: [String, NilClass], default: nil
 
 # Specify the config file template, destination on the node, and variables
 # If :config_template is nil, do nothing
-attribute :config_template, 'kind_of' => [String, NilClass],
-                            :default => 'settings.py.erb'
+attribute :config_template, kind_of: [String, NilClass],
+                            default: 'settings.py.erb'
 # The destination is an absolute path, or nil
-attribute :config_destination, 'kind_of' => [String, NilClass], :default => nil
-attribute :config_vars, 'kind_of' => Hash
+attribute :config_destination, kind_of: [String, NilClass], default: nil
+attribute :config_vars, kind_of: Hash
 
 # Port gunicorn will run on. If not nil, supervisor and gunicorn are installed
-attribute :gunicorn_port, 'kind_of' => [Integer, NilClass], :default => nil
-attribute :wsgi_module, 'kind_of' => [String, NilClass], :default => nil
+attribute :gunicorn_port, kind_of: [Integer, NilClass], default: nil
+attribute :wsgi_module, kind_of: [String, NilClass], default: nil
 
 # If this is set, install that requirements file. If it is not, install a
 # setup.py. If the setup.py does not exist, fail.
-attribute :requirements_file, 'kind_of' => [String, NilClass], :default => nil
+attribute :requirements_file, kind_of: [String, NilClass], default: nil
 
 # These attributes will only work for Django apps
-attribute :django_migrate, 'kind_of' => [TrueClass, FalseClass],
-                           :default => false
+attribute :django_migrate, kind_of: [TrueClass, FalseClass],
+                           default: false
 
-attribute :django_collectstatic, 'kind_of' => [TrueClass, FalseClass],
-                                 :default => false
+attribute :django_collectstatic, kind_of: [TrueClass, FalseClass],
+                                 default: false
 
-attribute :interpreter, 'kind_of' => String, :default => 'python'
+attribute :interpreter, kind_of: String, default: 'python'
